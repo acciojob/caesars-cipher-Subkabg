3469fe1725bee5d4fc401a5125b9b13309898e53
Subikabg <92627736+Subkabg@users.noreply.github.com>
2023-11-29 11:37:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -11,21 +11,16 @@ const lookup = {
 };
 
 function rot13(encodedStr){
-   let decodedArr = []; // Your Result goes here
-  // Only change code below this line
-for(let i=0; i<encodedStr.length; i++){
-	if(lookup[encodedStr[i]] === undefined){
-		decodedArr.push (encodedStr[i]);
+  let decodedstr = '';
+	for(let i=0; i<encodedStr.length; i++){
+		const char = encodedStr[i];
+		 decodedStr += lookup[char] || char;
+		
+	if(encodedStr.charAt[i] >= 'A' || charAt[i] <= 'Z'){
+		decodedstr = decodedstr + lookup[encodedStr.charAt[i]] || encodedStr.charAt[i];
 	}
-	else{
-		decodedArr.push(lookup[encodedStr[i]]);
-	}
-}
-  return decodedArr.join("");//return decodedArr
+	return decodedstr;
 }
 
-// You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
-// console.log(rot13("SERR YBIR? NPPVBWBO"));
 
 module.exports = rot13;
